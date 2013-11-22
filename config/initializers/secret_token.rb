abf25044b023f316619e0e3ddafd9cf497c8bbd1
James Davis <jamesmdavis5@gmail.com>
2013-11-22 09:16:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SilenceApp::Application.config.secret_token = '31428f53b6b7b9bc6e9436d63128e2fd510bd05ba7f441772c471acbb3b38cfc06d515c77cc907bac05fc51f937921b6f45d730d98f986b5f96c5dc3248e7fff'
